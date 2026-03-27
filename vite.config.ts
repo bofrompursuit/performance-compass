af78ab59be2e361d76bfbea43232316974b5b8e0
Bo <58dbghr6bk@privaterelay.appleid.com>
2026-03-27 13:53:33 -0400

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/performance-compass/", // <--- This tells the site its home is in this folder
+  base: "/performance-compass/",
   server: {
     host: "::",
     port: 8080,
