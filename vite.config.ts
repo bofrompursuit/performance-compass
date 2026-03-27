2ed496ec65f4a12853e96bb3578ca6b1bf2360f3
Bo <58dbghr6bk@privaterelay.appleid.com>
2026-03-27 13:59:38 -0400

    Simplify server configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/performance-compass/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  server: { host: "::", port: 8080 },
   plugins: [react()],
   resolve: {
     alias: {
